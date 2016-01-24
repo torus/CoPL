317f4303a75be88e36e71acd00714a58359e276a
Hisai Toru <toru@torus.jp>
2016-01-24 16:29:12 +0900

    chap. 4

--- ex41.c
+++ ex41.c
@@ -1,11 +1,4 @@
-// 35
-x = true, y = 4 |- if x then y + 1 else y - 1 evalto 5 by E-IfT {
-            x = true, y = 4 |- x evalto true by E-Var2 {
-                x = true |- x evalto true by E-Var1 {}
-            };
-            x = true, y = 4 |- y + 1 evalto 5 by E-Plus{
-                x = true, y = 4 |- y evalto 4 by E-Var1 {};
-                x = true, y = 4 |- 1 evalto 1 by E-Int {};
-                4 plus 1 is 5 by B-Plus {}
-            }
-        }
+|- let y = 2 in fun x -> x + y evalto (y=2)[fun x -> x + y] by E-Let {
+     |- 2 evalto 2 by E-Int {};
+     y = 2 |- fun x -> x + y evalto (y=2)[fun x -> x + y] by E-Fun {}
+}
